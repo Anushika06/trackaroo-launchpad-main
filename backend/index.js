da02ed4ac0586276e9b7907a9f10727d2b89b621
Anushika/06 <anushika.24bcs10344@sst.scaler.com>
2025-09-27 05:42:33 +0530

    changes in backend

--- backend/index.js
+++ backend/index.js
@@ -50,6 +50,9 @@ app.post('/', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server connected');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
